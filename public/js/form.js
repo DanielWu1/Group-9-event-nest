09712994edec60e6439262515624e10e98db1c42
prajay24 <67772724+prajay24@users.noreply.github.com>
2021-12-11 18:23:42 -0500

    changes for filter by category

--- public/js/form.js
+++ public/js/form.js
@@ -77,14 +77,7 @@
     })
     })
 
-    
-
-   
-
-
-})(window.jQuery);
-
- // filter button
+     // filter button
  $('#filterbtn').on('click',function(e){
 
     e.preventDefault();
@@ -109,57 +102,81 @@
         filterArr.push('Networking session');
     }
     
-    console.log(filterArr)
+    // console.log(filterArr)
 
-    let requestConfig = {
-        method: 'POST',
-        url: '/filterevents',
-        // data: JSON.stringify(
-        //     {filterList : filterArr}
-        // )
-        data: {filterList:filterArr}
+    
+
+    if(filterArr == undefined || filterArr == null || filterArr.length === 0){
+        alert("You have to input something");
+    } else {
+        let requestConfig = {
+            method: 'POST',
+            url: '/filterevents',
+            // data: JSON.stringify(
+            //     {filterList : filterArr}
+            // )
+            data: {filterList:filterArr}
+        }
+
+        console.log(requestConfig);
+
+        serchevent.children().each(function (index, element) {
+            element.remove();
+        })
+    
+        $.ajax(requestConfig).then(function (res){
+    
+    
+        // alert(res)
+    
+            let newelement = res
+                console.log(newelement)
+    
+                if (newelement.length === 0){
+                    let myeach = '<center><p class="container3"> not found by ' +mysearch+'</p></center>'
+                    serchevent.append(myeach)
+                }
+                else{
+                    for(let i =0 ; i <newelement.length; i++){
+                        let showdetail = newelement[i]
+                        // console.log(showdetail)
+                        // TODO: cherry for formatting?
+                        let myeach1 = '<center><p class="container3">' +showdetail.title+'</p></center>'
+                        let myeach2 = '<center><p class="container3">Category:    ' +showdetail.category+'</p></center>'
+                        let myeach3 = '<center><p class="container3">Creator Name:    ' +showdetail.creator+'</p></center>'
+                        let myeach4 = '<center><p class="container3">Date:    ' +showdetail.date+'</p></center>'
+                        let myeach5 = '<center><p class="container3">Start Time:    ' +showdetail.timestart+'</p></center>'
+                        let myeach6 = '<center><p class="container3">End Time:    ' +showdetail.endtime+'</p></center>'
+                        let myeach7 = '<center><p class="container3">Address:    ' +showdetail.address+'</p></center>'
+                        let myeach8 = '<center><p class="container3">City:    ' +showdetail.city+'</p></center>'
+                        let myeach9 = '<center><p class="container3">State:    ' +showdetail.state+'</p></center>'
+                        let myeach10 = '<center><p class="container3">Description:    ' +showdetail.description+'</p></center>'
+                        let myeach11 = '<center><p class="container4">Ticket Price:    ' +showdetail.price+'</p></center>'
+                        serchevent.append(myeach1,myeach2,myeach3,myeach4,myeach5,myeach6,myeach7,myeach8,myeach9,myeach10,myeach11)
+                        // console.log(serchevent)
+                    }
+    
+                }
+    
+                // serchevent.show()
+    
+                myallevent.hide()
+                serchevent.show()
+                homelink.show()
+    
+            
+        })
+
 
     }
 
-    console.log(requestConfig);
-
-    $.ajax(requestConfig).then(function (res){
-
-
-        alert(res)
-
-        let newelement = res
-            console.log(newelement)
-
-            if (newelement.length === 0){
-                let myeach = '<center><p class="container3"> not found by ' +mysearch+'</p></center>'
-                serchevent.append(myeach)
-            }
-            else{
-                for(let i =0 ; i <newelement.length; i++){
-            	    let showdetail = newelement[i]
-                    // console.log(showdetail)
-            	    let myeach1 = '<center><p class="container3">' +showdetail.title+'</p></center>'
-                    let myeach2 = '<center><p class="container3">Category:    ' +showdetail.category+'</p></center>'
-                    let myeach3 = '<center><p class="container3">Creator Name:    ' +showdetail.creator+'</p></center>'
-                    let myeach4 = '<center><p class="container3">Date:    ' +showdetail.date+'</p></center>'
-                    let myeach5 = '<center><p class="container3">Start Time:    ' +showdetail.timestart+'</p></center>'
-                    let myeach6 = '<center><p class="container3">End Time:    ' +showdetail.endtime+'</p></center>'
-                    let myeach7 = '<center><p class="container3">Address:    ' +showdetail.address+'</p></center>'
-                    let myeach8 = '<center><p class="container3">City:    ' +showdetail.city+'</p></center>'
-                    let myeach9 = '<center><p class="container3">State:    ' +showdetail.state+'</p></center>'
-                    let myeach10 = '<center><p class="container3">Description:    ' +showdetail.description+'</p></center>'
-                    let myeach11 = '<center><p class="container4">Ticket Price:    ' +showdetail.price+'</p></center>'
-            	    serchevent.append(myeach1,myeach2,myeach3,myeach4,myeach5,myeach6,myeach7,myeach8,myeach9,myeach10,myeach11)
-                    // console.log(serchevent)
-        	    }
-
-            }
-
-            serchevent.show()
-
-        
-    })
+   
     
 
 })
+
+   
+
+
+})(window.jQuery);
+
